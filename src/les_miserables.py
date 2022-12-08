960f78478a9e952bb8ce3f15701b0100388155fd
A.Sudarshan <f20190744@pilani.bits-pilani.ac.in>
2022-12-08 18:49:58 +0530

    working for weighted graphs too

--- src/les_miserables.py
+++ src/les_miserables.py
@@ -43,10 +43,14 @@ def parse_args():
     """
     parser = argparse.ArgumentParser(description="Run node2vec.")
 
-    parser.add_argument('--input', nargs='?', default='moreno_lesmis/out.moreno_lesmis_lesmis',
+    # parser.add_argument('--input', nargs='?', default='moreno_lesmis/out.moreno_lesmis_lesmis',
+    #                     help='Input graph path')
+    parser.add_argument('--input', nargs='?', default='moreno_lesmis/out1.moreno_lesmis_lesmis',
                         help='Input graph path')
 
-    parser.add_argument('--output', nargs='?', default='moreno_lesmis/lesmis.emb',
+    # parser.add_argument('--output', nargs='?', default='moreno_lesmis/lesmis.emb',
+    #                     help='Embeddings path')
+    parser.add_argument('--output', nargs='?', default='moreno_lesmis/lesmis1.emb',
                         help='Embeddings path')
 
     parser.add_argument('--dimensions', type=int, default=16,
@@ -78,7 +82,8 @@ def parse_args():
 
     parser.add_argument('--unweighted', dest='unweighted', action='store_false')
 
-    parser.set_defaults(weighted=False)
+    # parser.set_defaults(weighted=False)
+    parser.set_defaults(weighted=True)
 
     parser.add_argument('--directed', dest='directed', action='store_true',
                         help='c.')
